32f8ac4a0f5b8c4c48826eb2d1404f814a80c8c5
Brian Tran <briantran.math@gmail.com>
2022-12-23 01:01:14 -0500

    fix stderr in execute()

--- Netcat.py
+++ Netcat.py
@@ -10,7 +10,7 @@ def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
         return
-    output = subprocess.check_output(shlex.split(cmd), stder=subprocess.STDOUT)
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
 
     return output.decode()
 
